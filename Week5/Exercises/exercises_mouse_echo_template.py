650ac5658ed8a2988e7dc6a287b029083e568957
Lei <Crescent-Saturn@users.noreply.github.com>
2016-04-20 21:39:31 -0400

    Create exercises_mouse_echo_template.py

---
+++ Week5/Exercises/exercises_mouse_echo_template.py
@@ -0,0 +1,21 @@
+# Echo mouse click in console
+
+###################################################
+# Student should enter code below
+def click(position):
+    print "Mouse click at "+ str(position)
+
+
+
+
+###################################################
+# Sample output
+
+#Mouse click at (104, 105)
+#Mouse click at (169, 175)
+#Mouse click at (197, 135)
+#Mouse click at (176, 111)
+#Mouse click at (121, 101)
+#Mouse click at (166, 208)
+#Mouse click at (257, 235)
+#Mouse click at (255, 235)
