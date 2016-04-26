8f8cba312bf888ae8f1cbddba3cb46027bf2b590
Lei <Crescent-Saturn@users.noreply.github.com>
2016-04-26 18:17:08 -0400

    Update canvas draw cards and exposed value

--- Week5/Exercises/examples-memory_template.py
+++ Week5/Exercises/examples-memory_template.py
@@ -2,10 +2,20 @@
 
 import simplegui
 import random
+    
+cards = range(8)
+cards.extend(range(8))
+
+w =50
+h =100
+
+exposed = range(16)
+for i in exposed:
+    exposed[i] = False
 
 # helper function to initialize globals
 def new_game():
-    pass  
+    pass
 
      
 # define event handlers
@@ -16,8 +26,19 @@ def mouseclick(pos):
                         
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
-    pass
-
+    global cards, exposed
+#    random.shuffle(cards)
+    for n in range(len(cards)):
+        n_p = w*n+10		# number position
+        c_p = [(n*w,0),((n+1)*w,0),((n+1)*w,h),(n*w,h)]		# card position
+        if exposed[n]:
+            canvas.draw_text(str(cards[n]), [n_p,70], 50, "White")
+        else:
+            canvas.draw_polygon(c_p, 2, 'White','Green')
+        
+#    random.shuffle(cards)
+    
+    
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
@@ -33,3 +54,4 @@ new_game()
 frame.start()
 
 
+# Always remember to review the grading rubric
